1a644ab7cbbc9ac1b45e78e0b9bb467148b93f44
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-08-21 18:04:05 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -19,7 +19,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search needs an argument bruuh!")
             return
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("Searching....")
@@ -28,9 +28,9 @@ async def ytsearch(_, message: Message):
         text = ""
         while i < 4:
             text += f"**Title -** `{results[i]['title']}`\n"
-            text += f"**Duration -** `{results[i]['duration']}`\n"
             text += f"**Views -** `{results[i]['views']}`\n"
             text += f"**Channel -** `{results[i]['channel']}`\n"
+            text += f"**Duration -** `{results[i]['duration']}`\n"
             text += f"\nhttps://youtube.com{results[i]['url_suffix']}"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
